6d672450df777da7b995479fd8ec27036c6d4b76
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2024-10-04 18:17:25 +0200

    Added basic page routing

---
+++ src/pages/how-it-functions/cooks.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const cooks = () => {
+  return <div>cooks</div>;
+};
+
+export default cooks;
---
+++ src/pages/how-it-functions/foodies.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const foodies = () => {
+  return <div>foodies</div>;
+};
+
+export default foodies;
