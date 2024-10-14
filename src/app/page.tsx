81eb8c4f065d572ae4f30639cda448cf0a962fd5
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2024-10-14 20:14:35 +0200

    Added Banner Component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import Banner from './components/Banner';
+import bannerImg from '../../images/tavche-gravche.webp';
 export default function Home() {
+  const bannerProps = {
+    title: 'Јади Домашно',
+    subtitle: 'Вкусот на вашето соседство',
+    cities: ['Битола', 'Прилеп'],
+  };
   return (
     <div>
-      <main>
-        <p>OK</p>
-      </main>
+      <Banner
+        title={bannerProps.title}
+        subtitle={bannerProps.subtitle}
+        cities={bannerProps.cities}
+        image={bannerImg.src}
+      />
     </div>
   );
 }
