d12efac930e06e3eefc8a610f74740aaa15d921b
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2024-10-14 20:14:52 +0200

    Updates regarding new components

---
+++ src/app/interfaces/componentInterfaces.ts
@@ -0,0 +1,6 @@
+export interface BannerProps {
+  title: string;
+  subtitle: string;
+  image: string;
+  cities: string[];
+}
--- src/app/interfaces/generalInterfaces.ts
+++ src/app/interfaces/generalInterfaces.ts
@@ -13,3 +13,30 @@ export interface FooterLinks {
   label: string;
   href: string;
 }
+export interface Chef {
+  name: string;
+  surname: string;
+  foodType: string[];
+  starRated: number;
+  city: string;
+  image: {
+    url: string;
+    altText: string;
+  };
+  mainDish: {
+    name: string;
+    ingredients: string[];
+    price: number;
+    starRated: number;
+    description: string;
+    allergens: string[];
+  }[];
+  dishes: {
+    name: string;
+    ingredients: string[];
+    price: number;
+    starRated: number;
+    description: string;
+    allergens: string[];
+  }[];
+}
